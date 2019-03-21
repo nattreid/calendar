28121871d1964dd8278af08784015d7219ac9a79
attreid <attreid@gmail.com>
2019-03-21 09:34:17 +0100

    update

--- src/Calendar.php
+++ src/Calendar.php
@@ -31,6 +31,9 @@ class Calendar extends Control
 	/** @var bool */
 	private $showOtherDays = false;
 
+	/** @var int */
+	private $numberOfMonths = 1;
+
 	public function __construct()
 	{
 		parent::__construct();
@@ -38,31 +41,44 @@ class Calendar extends Control
 		$this->current = new DateTimeImmutable();
 	}
 
-	public function setFirstDayOfWeek(int $day)
+	public function setFirstDayOfWeek(int $day): self
 	{
 		if ($day < 0 || $day > 6) {
 			throw new InvalidArgumentException();
 		}
 		$this->firstDayOfWeek = $day;
+		return $this;
 	}
 
-	public function setCurrentDate(DateTimeInterface $date): void
+	public function setNumberOfMonths(int $months): self
+	{
+		if ($months < 1) {
+			throw new InvalidArgumentException();
+		}
+		$this->numberOfMonths = $months;
+		return $this;
+	}
+
+	public function setCurrentDate(DateTimeInterface $date): self
 	{
 		if ($date instanceof DateTime) {
 			$this->current = DateTimeImmutable::createFromMutable($date);
 		} else {
 			$this->current = $date;
 		}
+		return $this;
 	}
 
-	public function setShowOtherDays($show = true): void
+	public function setShowOtherDays($show = true): self
 	{
 		$this->showOtherDays = $show;
+		return $this;
 	}
 
-	public function setTranslator(ITranslator $translator): void
+	public function setTranslator(ITranslator $translator): self
 	{
 		$this->translator = $translator;
+		return $this;
 	}
 
 	public function getTranslator(): Translator
@@ -84,8 +100,18 @@ class Calendar extends Control
 		$this->template->addFilter('translate', [$this->translator, 'translate']);
 
 		$this->template->showOtherDays = $this->showOtherDays;
+		$this->template->numberOfMonths = $this->numberOfMonths;
 		$this->template->days = $this->getDays();
-		$this->template->date = new DateIterator($this->current, $this->firstDayOfWeek);
+
+		$dateIterator = [];
+		for ($i = 0; $i < $this->numberOfMonths; $i++) {
+			$date = $this->current;
+			if ($i > 0) {
+				$date = $date->modify($i . ' MONTH');
+			}
+			$dateIterator[] = new DateIterator($date, $this->firstDayOfWeek);
+		}
+		$this->template->dateIterator = $dateIterator;
 
 		$this->template->setFile(__DIR__ . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR . 'calendar.latte');
 		$this->template->render();
